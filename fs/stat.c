144fc885c6c094bfeb2b1adb09b1278e0497f620
Firxp011 <113378946+Firxp011@users.noreply.github.com>
2025-08-19 12:41:51 +0000

    Revert "Intregrate SukiSU tracepoint hooks"
    This reverts commit 1fd5d24744e4df0be3a35f2d6ab56a1811774240.

--- fs/stat.c
+++ fs/stat.c
@@ -17,9 +17,6 @@
 
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-#include <../drivers/kernelsu/ksu_trace.h>
-#endif
 
 void generic_fillattr(struct inode *inode, struct kstat *stat)
 {
@@ -297,10 +294,6 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-	trace_ksu_trace_stat_hook(&dfd, &filename, &flag);
-#endif
-
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
@@ -442,10 +435,6 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
-
-#if defined(CONFIG_KSU) && defined(CONFIG_KSU_TRACEPOINT_HOOK)
-	trace_ksu_trace_stat_hook(&dfd, &filename, &flag); /* 32-bit su support */
-#endif
 
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
